fd8d9ffe523b09a5af662639696006021327aa05
konikasingh <konikasingh1996@gmail.com>
2021-10-12 20:54:21 +0530

    Welcome

---
+++ LineProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
